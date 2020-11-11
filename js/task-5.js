6251fd305b2a76705457b2cfa4ce9e9136c8e26a
Yurii Berezynets <yuriiberezynets@gmail.com>
2020-11-11 20:11:16 +0200

    bla bla

--- js/task-5.js
+++ js/task-5.js
@@ -6,27 +6,47 @@ let price;
 switch (country.toLowerCase()) {
   case 'китай':
     price = 100;
-    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+    alert(
+      `Доставка в ${
+        countryToLowerCase[0].toUpperCase() + countryToLowerCase.slice(1)
+      } будет стоить ${price} кредитов`,
+    );
     break;
 
   case 'чили':
     price = 250;
-    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+    alert(
+      `Доставка в ${
+        countryToLowerCase[0].toUpperCase() + countryToLowerCase.slice(1)
+      } будет стоить ${price} кредитов`,
+    );
     break;
 
   case 'австралия':
     price = 170;
-    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+    alert(
+      `Доставка в ${
+        countryToLowerCase[0].toUpperCase() + countryToLowerCase.slice(1)
+      } будет стоить ${price} кредитов`,
+    );
     break;
 
   case 'индия':
     price = 80;
-    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+    alert(
+      `Доставка в ${
+        countryToLowerCase[0].toUpperCase() + countryToLowerCase.slice(1)
+      } будет стоить ${price} кредитов`,
+    );
     break;
 
   case 'ямайка':
     price = 120;
-    alert(`Доставка в ${country} будет стоить ${price} кредитов`);
+    alert(
+      `Доставка в ${
+        countryToLowerCase[0].toUpperCase() + countryToLowerCase.slice(1)
+      } будет стоить ${price} кредитов`,
+    );
     break;
 
   default:
